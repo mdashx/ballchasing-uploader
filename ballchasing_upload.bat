91080dba8eec81bcbc8f5111ff8328fbc2947750
Tom <tom@mdashx.com>
2021-08-28 13:56:19 -0600

    Set source_path correctly

--- ballchasing_upload.bat
+++ ballchasing_upload.bat
@@ -1,6 +1,6 @@
 @echo off
 SET filename=%~dp0%replay_dir.txt
-FOR /F "tokens=* delims=" %%x in (%filename%) DO SET api_key=%replay_dir%%%x
+FOR /F "tokens=* delims=" %%x in (%filename%) DO SET source_path=%source_path%%%x
 cd %source_path%
 :loop
 	forfiles /D +0 /C "cmd /c CALL %~dp0%upload_single_file.bat @file"
